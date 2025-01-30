941627925ee612b9325b3b26deb333b778d8e68b
Lucas Idiart <lucas.idiart@jala.university>
2025-01-30 18:12:59 -0300

    corrección de error para evitar ejecución duplicada.

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".button-add").addEventListener("click", agregarAmigo);
-    document.querySelector(".button-draw").addEventListener("click", sortearAmigo);
-});
-
 function agregarAmigo() {
     let nInput = document.getElementById("amigo");
     let nombre = nInput.value.trim();
