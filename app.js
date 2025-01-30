416d36567f3a89da9d3a90761f9ad8ae43abd0a6
Lucas Idiart <lucas.idiart@jala.university>
2025-01-30 18:03:52 -0300

    crear array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".button-add").addEventListener("click", agregarAmigo);
+    document.querySelector(".button-draw").addEventListener("click", sortearAmigo);
+});
+
